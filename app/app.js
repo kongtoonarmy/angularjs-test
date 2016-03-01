5aa1b8f3c5670d043efadd55eee374dc162d7b27
kongtoonarmy <kongtoonarmy@hotmail.com>
2016-03-01 20:53:12 +0700

    Using ui-router

---
+++ app/app.js
@@ -0,0 +1,17 @@
+angular.module('app', ['ui.router'])
+
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+        $stateProvider
+        .state('home', {
+            url: '/',
+            views: {
+                '': { templateUrl: 'templates/main.html'},
+                'nav@home': { templateUrl: 'templates/assets/nav.html' },
+                'body@home': { templateUrl: 'templates/body.html'},
+                'footer@home': { templateUrl: 'templates/assets/footer.html' }
+            }
+        });
+    }
+]);
