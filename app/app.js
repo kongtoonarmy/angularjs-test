935ecabf3e7991cc353f7f30bbdd08e0b6ad1afd
Kongyot Rojphutthikul <kong@freshconsulting.com>
2016-06-08 18:57:36 +0700

    Started to use ng-tags-input

---
+++ app/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('app', ['ngTagsInput']);
+
+app.controller('MainCtrl', function($scope, $http) {
+
+    /*$scope.tags = [
+        { text: 'Tag-Json-1' },
+        { text: 'Tag-Json-2' },
+        { text: 'Tag-Json-3' },
+        { text: 'Tag-Json-4' },
+        { text: 'Tag-Json-5' }
+    ];*/
+
+    $scope.tags = [
+            
+    ];
+
+    $scope.loadTags = function(query) {
+        return $http.get('/data/tags.json');
+        //return $scope.tags;
+    };
+});
