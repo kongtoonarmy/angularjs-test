9fd65d17764baa3e29b19855f9c981617841c30b
Kongyot Rojphutthikul <kongtoonarmy@hotmail.com>
2016-08-09 14:29:16 +0700

    Show content on Grid layout

--- app/app.js
+++ app/app.js
@@ -1,6 +1,50 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['akoenig.deckgrid']);
 
 app.controller('MainCtrl', function($scope) {
 
-    $scope.title = "Hello World";
+    $scope.photos = [
+        {
+        	id: 'photo-1', 
+        	name: 'Awesome photo', 
+        	src: 'http://localhost:8080/assets/images/nature-1.jpg',
+        	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit recusandae, distinctio blanditiis consequuntur eaque voluptas dolor fuga sit minima, culpa ratione debitis commodi nesciunt totam? Sed dolorum nihil perferendis vero.'
+        },
+        {
+        	id: 'photo-2', 
+        	name: 'Great photo', 
+        	src: 'http://localhost:8080/assets/images/abstract-1.jpg',
+        	description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, minus aperiam eaque nam cupiditate doloribus, nemo quo expedita quisquam consectetur placeat fuga sapiente porro laboriosam in molestiae quas error? Natus.'
+        },
+        {
+        	id: 'photo-3', 
+        	name: 'Strange photo', 
+        	src: 'http://localhost:8080/assets/images/abstract-2.jpg',
+        	description: '<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit placeat consequatur aperiam voluptatem ex, nostrum sunt ad incidunt suscipit doloremque voluptatum deleniti natus, sequi aliquid, porro deserunt ducimus molestiae! Pariatur?</div><div>Debitis voluptatibus quam, cum voluptatem, vel sapiente similique architecto. Perferendis maxime accusantium veniam delectus dolores, natus quasi tenetur, obcaecati doloribus magni quae tempore ea, consectetur nisi quibusdam cupiditate. Repudiandae, eaque.</div>'
+        },
+        {
+        	id: 'photo-4', 
+        	name: 'A photo?', 
+        	src: 'http://localhost:8080/assets/images/sports-1.jpg'
+        },
+        {
+        	id: 'photo-5', 
+        	name: 'What a photo', 
+        	src: 'http://localhost:8080/assets/images/sports-2.jpg'
+        },
+        {
+        	id: 'photo-6', 
+        	name: 'Awesome photo', 
+        	src: 'http://localhost:8080/assets/images/nature-1.jpg'
+        },
+        {
+        	id: 'photo-7', 
+        	name: 'Great photo', 
+        	src: 'http://localhost:8080/assets/images/abstract-1.jpg'
+        },
+        {
+        	id: 'photo-8', 
+        	name: 'Strange photo', 
+        	src: 'http://localhost:8080/assets/images/abstract-2.jpg'
+        }
+    ];
 });
